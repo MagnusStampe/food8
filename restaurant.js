a08948c9f1605e5e7c6a1e6a44f551f8e71436bb
MagnusStampe <m_s_nielsen288@hotmail.com>
2018-04-25 21:02:49 +0200

    HTML for restauranter fixed, js added

--- restaurant.js
+++ restaurant.js
@@ -13,11 +13,6 @@ function loaded() {
     hentRestaurant();
 }
 
-//insæt HTML i infobox
-function navListener(data) {
-    document.querySelector("[data-info]").innerHTML = info.acf["menukort" + data];
-    highlightNav(data);
-}
 // Hent restaurant HTML
 
 async function hentRestaurant() {
@@ -34,6 +29,8 @@ async function hentRestaurant() {
         rest = await jsonData.json();
 
         jsonInput();
+
+
 
     }
 
@@ -84,7 +81,7 @@ async function hentRestaurant() {
                     if (jsonLink != "") {
 
                         //Indsæt HTML i variabel
-                        navHtml = navHtml + "<button onclick=\"navListener('" + menuNr + "')\" data-nav-" + menuNr + ">" + restaurant.acf[nr2] + "</button>";
+                        navHtml = navHtml + "<button data-nav-" + menuNr + ">" + restaurant.acf[nr2] + "</button>";
 
                         function addListener() {
                             navListener(nr1);
@@ -98,44 +95,67 @@ async function hentRestaurant() {
 
                     } else {
                         document.querySelector("[data-nav]").innerHTML = navHtml;
+                        document.querySelector("[data-nav-om]").style.color = "#111";
 
-                        document.querySelector("[data-nav-om]").addEventListener("click", omHtml);
+                        document.querySelector("[data-nav-om]").addEventListener("click", () => {
 
-                        function omHtml() {
+
+                            let knapNr = 1;
+
+                            while (knapNr < menuNr) {
+                                document.querySelector("[data-nav-" + knapNr + "]").style.color = "#555";
+                                console.log(knapNr)
+                                knapNr++
+                            }
+                            document.querySelector("[data-nav-om]").style.color = "#111";
                             document.querySelector("[data-info]").innerHTML = restaurant.acf.om_restauranten;
-                            highlightNav("om");
+                        });
+                        document.querySelector("[data-nav-1]").addEventListener("click", () => {
+                            highlightBut("1");
+                        });
+                        document.querySelector("[data-nav-2]").addEventListener("click", () => {
+                            highlightBut("2");
+                        });
+                        document.querySelector("[data-nav-3]").addEventListener("click", () => {
+                            highlightBut("3");
+                        });
+                        document.querySelector("[data-nav-4]").addEventListener("click", () => {
+                            highlightBut("4");
+                        });
+                        document.querySelector("[data-nav-5]").addEventListener("click", () => {
+                            highlightBut("5");
+                        });
+                        document.querySelector("[data-nav-6]").addEventListener("click", () => {
+                            highlightBut("6");
+                        });
+                        document.querySelector("[data-nav-7]").addEventListener("click", () => {
+                            highlightBut("7");
+                        });
+                        document.querySelector("[data-nav-8]").addEventListener("click", () => {
+                            highlightBut("8");
+                        });
+                        document.querySelector("[data-nav-9]").addEventListener("click", () => {
+                            highlightBut("9");
+                        });
+                        document.querySelector("[data-nav-10]").addEventListener("click", () => {
+                            highlightBut("10");
+                        });
+
+                        function highlightBut(knap) {
+
+                            let knapNr = 1;
+
+                            document.querySelector("[data-nav-om]").style.color = "#555";
+                            while (knapNr < menuNr) {
+                                document.querySelector("[data-nav-" + knapNr + "]").style.color = "#555";
+                                console.log(knapNr)
+                                knapNr++
+                            }
+                            document.querySelector("[data-nav-" + knap + "]").style.color = "#111";
+
+                            document.querySelector("[data-info]").innerHTML = restaurant.acf["menukort" + knap];
                         }
                     }
-                    document.querySelector("[data-nav-1]").addEventListener("click", () => {
-                        highlightNav("1")
-                    });
-                    document.querySelector("[data-nav-2]").addEventListener("click", () => {
-                        highlightNav("2")
-                    });
-                    document.querySelector("[data-nav-3]").addEventListener("click", () => {
-                        highlightNav("3")
-                    });
-                    document.querySelector("[data-nav-4]").addEventListener("click", () => {
-                        highlightNav("4")
-                    });
-                    document.querySelector("[data-nav-5]").addEventListener("click", () => {
-                        highlightNav("5")
-                    });
-                    document.querySelector("[data-nav-6]").addEventListener("click", () => {
-                        highlightNav("6")
-                    });
-                    document.querySelector("[data-nav-7]").addEventListener("click", () => {
-                        highlightNav("7")
-                    });
-                    document.querySelector("[data-nav-8]").addEventListener("click", () => {
-                        highlightNav("8")
-                    });
-                    document.querySelector("[data-nav-9]").addEventListener("click", () => {
-                        highlightNav("9")
-                    });
-                    document.querySelector("[data-nav-10]").addEventListener("click", () => {
-                        highlightNav("10")
-                    });
                 }
 
             } else {
