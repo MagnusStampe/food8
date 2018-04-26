241d3df40ec85f0e58abe630f8aa99e3b4a19fed
MagnusStampe <m_s_nielsen288@hotmail.com>
2018-04-26 12:21:37 +0200

    farve og logo til restaurant

--- restaurant.js
+++ restaurant.js
@@ -1,28 +1,32 @@
-// Initial function
+//###########################################
+//SETUP - Setup af variabler og startfunktion
+//###########################################
 
+//Database restauranter
 let rest;
-let billede = 1;
+//Database enkelte restaurant
+let restaurant;
 
-let info;
-let highlightNew = "om";
-let highlightOld;
-
+// Initial function
 document.addEventListener("DOMContentLoaded", loaded);
 
 function loaded() {
     hentRestaurant();
 }
 
-// Hent restaurant HTML
-
+//#################################
+//HTML - Indsæt HTML til restaurant
+//#################################
 async function hentRestaurant() {
     let htmlData = await fetch("restaurant.html");
     let restaurantHtml = await htmlData.text();
     document.querySelector("main").innerHTML = restaurantHtml;
 
     hentJson();
-    // Hent JSON
 
+    //###############################
+    //JSON - Hent JSON til restaurant
+    //###############################
     async function hentJson() {
         let jsonData = await fetch("http://www.magnusstampe.dk/food8/wp/wp-json/acf/v3/restauranter");
         //let jsonData = await fetch("test.json");
@@ -30,102 +34,105 @@ async function hentRestaurant() {
 
         jsonInput();
 
-
-
     }
 
-    function highlightNav(high) {
-        highlightOld = highlightNew;
-        highlightNew = high;
-
-        console.log("[data-nav-" + highlightNew + "]");
-
-        document.querySelector("[data-nav-" + highlightNew + "]").classList.add("highlight");
-        document.querySelector("[data-nav-" + highlightOld + "]").classList.remove("highlight");
-    }
-
-
+    //###############################
+    //JSON - Indsæt JSONs HTML i HTML
+    //###############################
     function jsonInput() {
         rest.forEach(restaurant => {
-            let restaurantNavn = document.getElementById("header").textContent;
 
+            //Find restaurantens navn fra <h1 id="header">NAVN PÅ RESTAURANT</h1>
+            restaurantNavn = document.getElementById("header").textContent;
+
+            //Sammenligner restaurantnavn fra databasen med navn fra #header, tjek console.log fra efter "else" hvis der er problemer
             if (restaurantNavn == restaurant.acf.restaurant_navn) {
                 console.log("Match: " + restaurantNavn + " fundet");
 
+                //Indsæt tekst til "om" i "data-info"
                 document.querySelector("[data-info]").innerHTML = restaurant.acf.om_restauranten;
+                //Indsæt restaurantens adresse
                 document.querySelector("[data-address]").innerHTML = restaurant.acf.adresse;
+                //Indsæt telefonnummer hvis der er et
                 if (restaurant.acf.nummer != "") {
                     document.querySelector("[data-nummer]").innerHTML = "Tlf: +45 " + restaurant.acf.nummer;
                 }
+                //Indsæt åbningstider
                 document.querySelector("[data-abningstider]").innerHTML = restaurant.acf.åbningstider;
 
-                //####
-                //infobox
-                //####
 
-                //HTML
-                let navHtml = "<button data-nav-om>Om</button>";
-
-                //Om restaurant
-                let om = restaurant.acf.om_restauranten;
-
-
-                //Menukort
+                //##################################
+                //NAV HTML - Samling af HTML til NAV
+                //##################################
                 let menuNr = "1";
                 let menukort = "menukort" + menuNr;
                 let menukortNavn = "navn_pa_menukort" + menuNr;
+                let navHtml = "<button data-nav-om>Om</button>";
+
+                //Ret logo i header til restaurantens logo
+                document.querySelector("#header_bar img").src = restaurant.acf.logo.url;
+                document.querySelector("#header_bar img").alt = restaurant.acf.restaurantens_navn;
+                //Ret headerens farve til restaurantens farve
+                document.querySelector("#header_bar").style.backgroundColor = restaurant.acf.farve;
+                document.querySelector("header nav").style.backgroundColor = restaurant.acf.farve;
 
                 indsaetMenukort(menukort, menukortNavn);
 
                 function indsaetMenukort(nr1, nr2) {
+                    //JSON stig til menukortet
                     let jsonLink = restaurant.acf[nr1];
-                    info = restaurant;
 
+                    //Indsæt menupunkter indtil der ikke er flere
                     if (jsonLink != "") {
 
                         //Indsæt HTML i variabel
                         navHtml = navHtml + "<button data-nav-" + menuNr + ">" + restaurant.acf[nr2] + "</button>";
 
-                        function addListener() {
-                            navListener(nr1);
-                        }
-
                         //Gør klar til næste menukort
                         menuNr++;
                         let menukort = "menukort" + menuNr;
                         let menukortNavn = "navn_pa_menukort" + menuNr;
+                        //Start "indseatMenukort()" forfra
                         indsaetMenukort(menukort, menukortNavn);
 
                     } else {
+                        //#################################################
+                        //NAV HTML - Indsæt HTML i NAV og highlight knapper
+                        //#################################################
                         document.querySelector("[data-nav]").innerHTML = navHtml + "<button data-nav-kontrol>Kontrolrapport</button>";
-                        document.querySelector("[data-nav-om]").style.color = "#111";
 
+                        let selectedColor = "#111";
+                        let notSelectedColor = "555"
+
+                        //Highlight "om"
+                        document.querySelector("[data-nav-om]").style.color = selectedColor;
+
+                        //EventListener til "Om"
                         document.querySelector("[data-nav-om]").addEventListener("click", () => {
-
-
                             let knapNr = 1;
 
                             while (knapNr < menuNr) {
-                                document.querySelector("[data-nav-" + knapNr + "]").style.color = "#555";
+                                document.querySelector("[data-nav-" + knapNr + "]").style.color = notSelectedColor;
                                 knapNr++
                             }
-                            document.querySelector("[data-nav-kontrol]").style.color = "#555";
-                            document.querySelector("[data-nav-om]").style.color = "#111";
+                            document.querySelector("[data-nav-kontrol]").style.color = notSelectedColor;
+                            document.querySelector("[data-nav-om]").style.color = selectedColor;
                             document.querySelector("[data-info]").innerHTML = restaurant.acf.om_restauranten;
                         });
+                        //EventListener til "Kontrolrapport"
                         document.querySelector("[data-nav-kontrol]").addEventListener("click", () => {
-
 
                             let knapNr = 1;
 
                             while (knapNr < menuNr) {
-                                document.querySelector("[data-nav-" + knapNr + "]").style.color = "#555";
+                                document.querySelector("[data-nav-" + knapNr + "]").style.color = notSelectedColor;
                                 knapNr++
                             }
-                            document.querySelector("[data-nav-om]").style.color = "#555";
-                            document.querySelector("[data-nav-kontrol]").style.color = "#111";
+                            document.querySelector("[data-nav-om]").style.color = notSelectedColor;
+                            document.querySelector("[data-nav-kontrol]").style.color = selectedColor;
                             document.querySelector("[data-info]").innerHTML = "<a href='" + restaurant.acf.kontrolrapport + "' target='_blank'>" + restaurant.acf.kontrolrapport + "</a>";
                         });
+                        //EventListeners til menupunkter
                         document.querySelector("[data-nav-1]").addEventListener("click", () => {
                             highlightBut("1");
                         });
@@ -157,6 +164,7 @@ async function hentRestaurant() {
                             highlightBut("10");
                         });
 
+                        //function() til at highlighte menupunktsknapper
                         function highlightBut(knap) {
 
                             let knapNr = 1;
@@ -175,6 +183,9 @@ async function hentRestaurant() {
                 }
 
             } else {
+                //###########################################
+                //INTET MATCH - console.log til at finde fejl
+                //###########################################
                 console.log("Intet match: " + restaurantNavn + " & " + restaurant.acf.restaurant_navn);
             }
         });
