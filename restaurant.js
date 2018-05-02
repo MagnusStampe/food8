f495e30a34de8c55c3475c6899821b05d3b507dd
Joachim Secher <joachimsecher@Joachims-MacBook-Pro.local>
2018-05-02 14:23:32 +0200

    wtf

--- restaurant.js
+++ restaurant.js
@@ -58,6 +58,54 @@ async function hentRestaurant() {
 
             //Sammenligner restaurantnavn fra databasen med navn fra #header, tjek console.log fra efter "else" hvis der er problemer
             if (restaurantNavn == restaurant.acf.restaurant_navn) {
+                //###############################
+                //--------------slider--------------
+                //###############################
+                /*
+                                slide();
+                                var myIndex = 0;
+                                showDivs();
+
+                                function showDivs() {
+                                    var i;
+                                    var x = document.getElementsByClassName("mySlides");
+                                    for (i = 0; i < x.length; i++) {
+                                        x[i].style.display = "none";
+                                    }
+                                    myIndex++;
+                                    if (myIndex > x.length) {
+                                        myIndex = 1
+                                    }
+                                    x[myIndex - 1].style.display = "block";
+                                    setTimeout(showDivs, 2000); // Change image every 2 seconds
+                                }
+
+                                function slide() {
+
+
+
+                                    let dest = document.querySelector("[data-buti-dest]");
+                                    let temp = document.querySelector("[data-template]");
+
+                                    let klon = temp.cloneNode(true).content;
+                                    let imgNummer = 1;
+                                    let imgLink = "billede " + imgNummer;
+                                    console.log(restaurant.acf[imgLink].url);
+                                    if (imgLink != "") {
+                                        //        //data i <div>
+
+                                        klon.querySelector("[data-buti-img]").src = restaurant.acf[imgLink].url;
+                                        klon.querySelector("[data-buti-img]").alt = restaurant.acf[imgLink].url;
+                                        dest.appendChild(klon);
+                                        imgNummer++;
+                                        slide();
+                                    }
+                                    //
+                                }
+
+                                */
+
+
 
 
                 //----------------Google maps-----------------
